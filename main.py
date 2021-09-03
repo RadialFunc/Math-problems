02b3af8d27c4e8b6959565136f577b94c324953b
Rohan Garg <84034088+RadialFunc@users.noreply.github.com>
2021-09-03 16:33:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import requests
 import os
 
 
-for n in range (2016, 2017):
+for n in range (2002, 2022):
 	#parent  = os.getcwd()
-	#path = parent+"/AMC12/B/"+str(n)
+	#path = parent+"/AMC10/A/"+str(n)
 	#os.makedirs(path)
-	for problem in range (9,26):
-		img_data = requests.get(f"https://ggim.me/images/AMC-12B-{n}-P{problem}").content
-		f= open(f'AMC12/B/{n}/{problem}.png', 'x')
-		f = open(f'AMC12/B/{n}/{problem}.png', 'wb')
+	for problem in range (1,26):
+		img_data = requests.get(f"https://ggim.me/images/AMC-10A-{n}-P{problem}").content
+		f= open(f'AMC10/A/{n}/{problem}.png', 'x')
+		f = open(f'AMC10/A/{n}/{problem}.png', 'wb')
 		f.write(img_data)
 
